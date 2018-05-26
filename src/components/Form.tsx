0bbb9674302cf8bb81614129012d76d0f3cb8796
km45 <km45@users.noreply.github.com>
2018-05-26 17:06:03 +0900

    Change form to log input.text when click button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,43 @@
 import * as React from "react";
 
-export interface HelloProps { compiler: string; framework: string; }
+export interface FormProps {
+    compiler: string;
+    framework: string;
+}
 
-export class Form extends React.Component<HelloProps, {}> {
-    onClick() {
-        console.log('clicked')
+class FormState {
+    text: string;
+}
+
+export class Form extends React.Component<FormProps, FormState> {
+    constructor(props: FormProps) {
+        super(props);
+        this.state = {
+            text: 'sample text'
+        };
+    }
+
+    onClick(event: React.MouseEvent<HTMLInputElement>) {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
+    onTextAreaChange(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            text: event.target.value
+        });
     }
 
     render() {
-        return <div><input type="text" /><button onClick={this.onClick}>change</button></div>;
+        return (
+            <form>
+                <input type="text"
+                    value={this.state.text}
+                    onChange={(event) => this.onTextAreaChange(event)} />
+                <input type="button"
+                    value="OK"
+                    onClick={(event) => this.onClick(event)} />
+            </form>
+        );
     }
 }
