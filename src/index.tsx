1dee320865a84340632073b45ec560d2b8b85bb3
km45 <km45@users.noreply.github.com>
2018-05-28 21:36:45 +0900

    Remove Hello.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
 import { Binder } from "./components/Binder";
 
 ReactDOM.render(
     <div>
-        <Hello compiler="TypeScript" framework="React" />
         <Binder />
     </div>,
     document.getElementById("example")
