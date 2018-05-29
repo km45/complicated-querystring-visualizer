a9ca9253f432c47595c0d22c45318aeff1101acd
km45 <km45@users.noreply.github.com>
2018-05-30 03:15:28 +0900

    Add ArrayRow, ArrayTable, ObjectRow and ObjectTable

--- src/ts/logic/converter.test.ts
+++ src/ts/logic/converter.test.ts
@@ -1,4 +1,8 @@
-import { generateQuery, parseQuery } from "./converter";
+import {
+    generateQuery, parseQuery,
+    arrayRowToObjectRow, arrayTableToObjectTable,
+    objectRowToArrayRow, objectTableToArrayTable
+} from "./converter";
 
 describe('LogicConverterTest', () => {
     it('GenerateQuery', () => {
@@ -24,6 +28,68 @@ describe('LogicConverterTest', () => {
         ];
 
         const actual = parseQuery(input);
+
+        expect(actual).toEqual(expected);
+    });
+    it('arrayRowToObjectRow', () => {
+        const keys = ['k1', 'k2'];
+        const row = ['v1', 'v2'];
+
+        const expected = {
+            k1: 'v1',
+            k2: 'v2'
+        };
+
+        const actual = arrayRowToObjectRow(keys, row);
+
+        expect(actual).toEqual(expected);
+    });
+    it('arrayTableToObjectTable', () => {
+        const column_kes = ['k1', 'k2'];
+        const rows = [
+            ['a1', 'a2'],
+            ['b1', 'b2'],
+            ['c1', 'c2']
+        ];
+
+        const expected = [
+            { k1: 'a1', k2: 'a2' },
+            { k1: 'b1', k2: 'b2' },
+            { k1: 'c1', k2: 'c2' }
+        ];
+
+        const actual = arrayTableToObjectTable(column_kes, rows);
+
+        expect(actual).toEqual(expected);
+    });
+    it('objectRowToArrayRow', () => {
+        const keys = ['k1', 'k2'];
+        const object = {
+            k1: 'v1',
+            k2: 'v2'
+        };
+
+        const expected = ['v1', 'v2'];
+
+        const actual = objectRowToArrayRow(keys, object);
+
+        expect(actual).toEqual(expected);
+    });
+    it('objectTableToArrayTable', () => {
+        const column_kes = ['k1', 'k2'];
+        const object = [
+            { k1: 'a1', k2: 'a2' },
+            { k1: 'b1', k2: 'b2' },
+            { k1: 'c1', k2: 'c2' }
+        ];
+
+        const expected = [
+            ['a1', 'a2'],
+            ['b1', 'b2'],
+            ['c1', 'c2'],
+        ];
+
+        const actual = objectTableToArrayTable(column_kes, object);
 
         expect(actual).toEqual(expected);
     });
--- src/ts/logic/converter.ts
+++ src/ts/logic/converter.ts
@@ -3,6 +3,12 @@ interface BasicParameter {
     value: string;
 }
 
+type ArrayRow = string[];
+type ArrayTable = ArrayRow[];
+
+type ObjectRow = { [key: string]: string };
+type ObjectTable = ObjectRow[];
+
 export function parseQuery(query: string): BasicParameter[] {
     return query.split('&').map(
         (element: string): BasicParameter => {
@@ -19,4 +25,36 @@ export function generateQuery(parameters: BasicParameter[]): string {
         (v: BasicParameter): string => {
             return v.key + '=' + v.value;
         }).join('&');
+}
+
+export function arrayRowToObjectRow(keys: string[], row: ArrayRow): ObjectRow {
+    let v: ObjectRow = {};
+    for (let i = 0; i < keys.length; ++i) {
+        v[keys[i]] = row[i];
+    }
+    return v;
+}
+
+export function arrayTableToObjectTable(column_keys: string[], table: ArrayTable): ObjectTable {
+    let v: ObjectTable = [];
+    for (let i = 0; i < table.length; ++i) {
+        v.push(arrayRowToObjectRow(column_keys, table[i]));
+    }
+    return v;
+}
+
+export function objectRowToArrayRow(keys: string[], obj: ObjectRow): ArrayRow {
+    let v: ArrayRow = [];
+    for (let i = 0; i < keys.length; ++i) {
+        v.push(obj[keys[i]]);
+    }
+    return v;
+}
+
+export function objectTableToArrayTable(column_keys: string[], obj: ObjectTable): ArrayTable {
+    let v: ArrayTable = [];
+    for (let i = 0; i < obj.length; ++i) {
+        v.push(objectRowToArrayRow(column_keys, obj[i]));
+    }
+    return v;
 }
