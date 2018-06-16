da8581917de1fe8f34338672709a6715f577beaa
km45 <km45@users.noreply.github.com>
2018-06-16 18:19:44 +0900

    Add table for host

--- src/ts/components/Binder.tsx
+++ src/ts/components/Binder.tsx
@@ -14,6 +14,8 @@ import {
     ColumnsDefinition
 } from "../logic/query-binder";
 
+import * as UrlBinder from '../logic/url-binder';
+
 export interface Props { }
 interface State { }
 
@@ -22,6 +24,7 @@ class BinderImplRef {
     form: React.RefObject<Form> = React.createRef();
     basic_grid: React.RefObject<Grid> = React.createRef();
     coord_grid: React.RefObject<Grid> = React.createRef();
+    host_grid: React.RefObject<Grid> = React.createRef();
 }
 
 export class Binder extends React.Component<Props, State> {
@@ -47,15 +50,21 @@ export class Binder extends React.Component<Props, State> {
             console.error('Unexpected null object');
             return;
         }
+        if (this.ref.host_grid.current == null) {
+            console.error('Unexpected null object');
+            return;
+        }
 
-        let query = this.ref.form.current.getText();
-        let parsed_result = parseQuery(query);
+        const url = this.ref.form.current.getText();
+        const parsed_result = UrlBinder.parseUrl(url);
         console.log(parsed_result);
 
         this.ref.basic_grid.current.setRows(arrayTableToObjectTable(
-            ColumnsDefinition.basic, parsed_result.basic));
+            ColumnsDefinition.basic, parsed_result.query.basic));
         this.ref.coord_grid.current.setRows(arrayTableToObjectTable(
-            ColumnsDefinition.coord, parsed_result.coord));
+            ColumnsDefinition.coord, parsed_result.query.coord));
+        this.ref.host_grid.current.setRows(arrayTableToObjectTable(
+            UrlBinder.ColumnsDefinition.host, parsed_result.host));
     }
 
     private onClickGridToForm(event: React.MouseEvent<HTMLInputElement>) {
@@ -74,18 +83,27 @@ export class Binder extends React.Component<Props, State> {
             console.error('Unexpected null object');
             return;
         }
+        if (this.ref.host_grid.current == null) {
+            console.error('Unexpected null object');
+            return;
+        }
 
-        const query = generateQuery({
-            basic: objectTableToArrayTable(
-                ColumnsDefinition.basic,
-                this.ref.basic_grid.current.getRows()),
-            coord: objectTableToArrayTable(
-                ColumnsDefinition.coord,
-                this.ref.coord_grid.current.getRows())
+        const url = UrlBinder.generateUrl({
+            host: objectTableToArrayTable(
+                UrlBinder.ColumnsDefinition.host,
+                this.ref.host_grid.current.getRows()),
+            query: {
+                basic: objectTableToArrayTable(
+                    ColumnsDefinition.basic,
+                    this.ref.basic_grid.current.getRows()),
+                coord: objectTableToArrayTable(
+                    ColumnsDefinition.coord,
+                    this.ref.coord_grid.current.getRows())
+            }
         });
-        console.log(query);
+        console.log(url);
 
-        this.ref.form.current.setText(query);
+        this.ref.form.current.setText(url);
     }
 
     public render() {
@@ -100,6 +118,10 @@ export class Binder extends React.Component<Props, State> {
                         value="grid2from"
                         onClick={(event) => this.onClickGridToForm(event)} />
                 </form>
+                <Grid
+                    columns={UrlBinder.ColumnsDefinition.host}
+                    title='Host'
+                    ref={this.ref.host_grid} />
                 <Grid
                     columns={ColumnsDefinition.basic}
                     title='Basic'
