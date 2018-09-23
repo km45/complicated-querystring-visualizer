da444dd74bccd51942e27f9cc557919a88c7d1de
km45 <km45@users.noreply.github.com>
2018-09-24 01:41:10 +0900

    Format webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,49 +2,49 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env, argv) => {
-    const mode = argv.mode;
-return {
-    entry: "./src/ts/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist/" + mode
-    },
+  const mode = argv.mode;
+  return {
+    entry: './src/ts/index.tsx',
+        output: {filename: 'bundle.js', path: __dirname + '/dist/' + mode},
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+        // Enable sourcemaps for debugging webpack's output.
+        devtool: 'source-map',
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+        resolve: {
+          // Add '.ts' and '.tsx' as resolvable extensions.
+          extensions: ['.ts', '.tsx', '.js', '.json']
+        },
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+        module: {
+          rules:
+              [
+                // All files with a '.ts' or '.tsx' extension will be handled by
+                // 'awesome-typescript-loader'.
+                {test: /\.tsx?$/, loader: 'awesome-typescript-loader'},
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
+                // All output '.js' files will have any sourcemaps re-processed
+                // by 'source-map-loader'.
+                {enforce: 'pre', test: /\.js$/, loader: 'source-map-loader'}
+              ]
+        },
 
-    externals: {
-        "ag-grid-community": "agGrid",
-        "ag-grid-react": false,
-        "pythonic": false,
-        "react": "React",
-        "react-dom": "ReactDOM",
-        "react-dom-factories": false,
-        "semantic-ui-react": "semanticUIReact"
-    },
+        externals: {
+          'ag-grid-community': 'agGrid',
+          'ag-grid-react': false,
+          'pythonic': false,
+          'react': 'React',
+          'react-dom': 'ReactDOM',
+          'react-dom-factories': false,
+          'semantic-ui-react': 'semanticUIReact'
+        },
 
-    plugins: [
-        new CleanWebpackPlugin([__dirname + "/dist/" + mode]),
-        new HtmlWebpackPlugin({
+        plugins: [
+          new CleanWebpackPlugin([__dirname + '/dist/' + mode]),
+          new HtmlWebpackPlugin({
             template: __dirname + '/src/html/index.html',
-            title: 'react-studies('+ mode +')'
-        })
-    ]
+            title: 'react-studies(' + mode + ')'
+          })
+        ]
 
     // Disabled following settings defined in webpack.production.js
     // because it is maybe needless:
@@ -59,5 +59,5 @@ return {
     //     })
     // ]
     // ```
-}
+  }
 }
