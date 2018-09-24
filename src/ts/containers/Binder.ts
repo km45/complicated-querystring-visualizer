fec43d3a0890d23ff757d6853dc57e0f70f58950
km45 <km45@users.noreply.github.com>
2018-09-25 00:21:43 +0900

    Fix lint errors

--- src/ts/containers/Binder.ts
+++ src/ts/containers/Binder.ts
@@ -3,6 +3,6 @@ import * as ReactRedux from 'react-redux';
 import {Binder} from '../components/Binder';
 import {BinderState} from '../reducer';
 
-const mapStateToProps = (state: BinderState) => state
+const mapStateToProps = (state: BinderState) => state;
 
 export default ReactRedux.connect(mapStateToProps)(Binder);
--- src/ts/containers/Form.ts
+++ src/ts/containers/Form.ts
@@ -3,6 +3,6 @@ import * as ReactRedux from 'react-redux';
 import {Form} from '../components/Form';
 import {FormState} from '../reducer';
 
-const mapStateToProps = (state: FormState) => state
+const mapStateToProps = (state: FormState) => state;
 
 export default ReactRedux.connect(mapStateToProps)(Form);
