2fd87bbd0303da7f4fcefad9a3e20f68bac6c2c4
km45 <km45@users.noreply.github.com>
2018-09-25 03:00:30 +0900

    Add comment

--- src/ts/modules/Form.ts
+++ src/ts/modules/Form.ts
@@ -1,8 +1,6 @@
 import * as TypescriptFsa from 'typescript-fsa';
 import * as TypescriptFsaReducers from 'typescript-fsa-reducers';
 
-const SET_TEXT = 'react-studies/Form/SET_TEXT'
-
 export interface FormState {
   text: string;
 }
@@ -15,10 +13,21 @@ function handler(state: FormState, payload: string): any {
   return {...state, text: payload};
 }
 
+// ----------------------------------------------------------------------------
+// action types
+// ----------------------------------------------------------------------------
+const SET_TEXT = 'react-studies/Form/SET_TEXT'
+
+// ----------------------------------------------------------------------------
+// action creators
+// ----------------------------------------------------------------------------
 const actionCreator = TypescriptFsa.actionCreatorFactory();
 
 export const setFormText = actionCreator<string>(SET_TEXT);
 
+// ----------------------------------------------------------------------------
+// reducer
+// ----------------------------------------------------------------------------
 export default TypescriptFsaReducers.reducerWithInitialState(initialReduceState)
     .case(setFormText, handler)
     .build();
