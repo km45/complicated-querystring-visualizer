73b4eda188c2ee5e03355cd01bff676472b944a6
km45 <km45@users.noreply.github.com>
2018-09-25 16:50:40 +0900

    Move Form props from components to containers

--- src/ts/components/Form.tsx
+++ src/ts/components/Form.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
-import * as Redux from 'redux';
 import * as SemanticUiReact from 'semantic-ui-react';
 
-import { FormState, setFormText } from '../modules/Form';
-
-export interface Props extends FormState {
-    dispatch: Redux.Dispatch<any>;
-}
+import { setFormText } from '../modules/Form';
+import { Props } from '../containers/Form';
 
 interface State { }
 
--- src/ts/containers/Form.ts
+++ src/ts/containers/Form.ts
@@ -1,8 +1,34 @@
 import * as ReactRedux from 'react-redux';
+import * as Redux from 'redux';
 
 import {Form} from '../components/Form';
 import {FormState} from '../modules/Form';
 
-const mapStateToProps = (state: FormState) => state;
+interface StateProps {
+  text: string;
+}
 
-export default ReactRedux.connect(mapStateToProps)(Form);
+interface DispatchProps {
+  dispatch: Redux.Dispatch<any>;
+}
+
+export interface Props {
+  dispatch: Redux.Dispatch<any>;
+  text: string;
+}
+
+function mapStateToProps(state: FormState): StateProps {
+  return {text: (state.text ? state.text : '')};
+}
+
+function mapDispatchToProps(dispatch: any): DispatchProps {
+  return {dispatch};
+}
+
+function mergeProps(
+    stateProps: StateProps, dispatchProps: DispatchProps, _: any): Props {
+  return {dispatch: dispatchProps.dispatch, text: stateProps.text};
+}
+
+export default ReactRedux.connect(
+    mapStateToProps, mapDispatchToProps, mergeProps)(Form);
