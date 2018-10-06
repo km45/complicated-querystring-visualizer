4e2fd0b19266a81b4bb9531be3032e893ee2a323
km45 <km45@users.noreply.github.com>
2018-10-06 19:51:38 +0900

    Add e2e test for issue #6

--- src/python/tests/test_end_to_end.py
+++ src/python/tests/test_end_to_end.py
@@ -94,20 +94,109 @@ class TestEndToEnd():
         assert basic_body_cells[1].text == 'c'
 
     def test_grid_data_reference_problem(self):
-        # TODO: Implement test for following problem:
-        # // Change reference for data even if contents are same,
-        # // otherwise following problem happens.
-        # //
-        # // 1. Parse url "a=b" for the first time.
-        # //   1.1. Allocate memory for parsed data. Call it "Data1".
-        # //   1.2. This class refers "Data1".
-        # //   1.3. AgGrid refers "Data1".
-        # // 2. Parse url "localhost/?a=b" for the second time.
-        # //   2.1. Allocate memory for parsed data. Call it "Data2".
-        # //   2.2. This class changes to refer "Data2"
-        # //        because setState() is called explicitly. (above)
-        # //   2.3. AgGrid remains referring "Data1"
-        # //        because contents are not changed.
-        # // 3. Edit grid data "Data2"
-        # // 4. Generate url from "Data1" and not used grid data.
-        pass
+        # Test for isssue #6
+        # https://github.com/km45/react-studies/issues/6
+        #
+        #
+        # Change reference for data even if contents are same,
+        # otherwise following problem will happen.
+        #
+        # 1. Parse url "a=b" for the first time.
+        #   1.1. Allocate memory for parsed data. Call it "Data1".
+        #   1.2. This class refers "Data1".
+        #   1.3. AgGrid refers "Data1".
+        # 2. Parse url "localhost/?a=b" for the second time.
+        #   2.1. Allocate memory for parsed data. Call it "Data2".
+        #   2.2. This class changes to refer "Data2"
+        #        because setState() is called explicitly. (above)
+        #   2.3. AgGrid remains referring "Data1"
+        #        because contents are not changed.
+        # 3. Edit grid data "Data2"
+        # 4. Generate url from "Data1" and not used grid data.
+
+        driver = self._driver
+
+        # Open page without query
+        driver.get('file://{}'.format(_FILEPATH))
+
+        # Alias
+        textarea = driver.find_element_by_tag_name('textarea')
+        generate_button = driver.find_element_by_xpath(_GENERATE_BUTTON_XPATH)
+        parse_button = driver.find_element_by_xpath(_PARSE_BUTTON_XPATH)
+
+        # Initial state
+        basic_body_cells = driver.find_elements_by_xpath(
+            _BASIC_PARAMETERS_PARSED_GRID_BODY_CELLS_XPATH)
+
+        assert textarea.text == ''
+        assert len(basic_body_cells) == 0
+
+        # ---------------------------------------------------------------------
+        # 1. Parse url "a=b" for the first time.
+        # ---------------------------------------------------------------------
+
+        # Enter query into textarea
+        textarea.clear()
+        textarea.send_keys('a=b')
+
+        assert textarea.text == 'a=b'
+
+        # Click parse button
+        parse_button.click()
+
+        basic_body_cells = driver.find_elements_by_xpath(
+            _BASIC_PARAMETERS_PARSED_GRID_BODY_CELLS_XPATH)
+
+        assert textarea.text == 'a=b'
+        assert len(basic_body_cells) == 2
+        assert basic_body_cells[0].text == 'a'
+        assert basic_body_cells[1].text == 'b'
+
+        # ---------------------------------------------------------------------
+        # 2. Parse url "localhost/?a=b" for the second time.
+        # ---------------------------------------------------------------------
+
+        # Enter query into textarea
+        textarea.clear()
+        textarea.send_keys('localhost/?a=b')
+
+        assert textarea.text == 'localhost/?a=b'
+
+        # Click parse button
+        parse_button.click()
+
+        basic_body_cells = driver.find_elements_by_xpath(
+            _BASIC_PARAMETERS_PARSED_GRID_BODY_CELLS_XPATH)
+
+        assert textarea.text == 'localhost/?a=b'
+        assert len(basic_body_cells) == 2
+        assert basic_body_cells[0].text == 'a'
+        assert basic_body_cells[1].text == 'b'
+
+        # ---------------------------------------------------------------------
+        # 3. Edit grid data "Data2"
+        # ---------------------------------------------------------------------
+
+        # Edit parsed query on grid
+        cell = basic_body_cells[1]
+        assert cell.text == 'b'
+
+        cell.click()
+        cell.send_keys('c')
+        cell.send_keys(Keys.ENTER)
+        assert cell.text == 'c'
+
+        # ---------------------------------------------------------------------
+        # 4. Generate url from "Data1" and not used grid data.
+        # ---------------------------------------------------------------------
+
+        # Click generate button
+        generate_button.click()
+
+        basic_body_cells = driver.find_elements_by_xpath(
+            _BASIC_PARAMETERS_PARSED_GRID_BODY_CELLS_XPATH)
+
+        assert textarea.text == 'localhost/?a=c'
+        assert len(basic_body_cells) == 2
+        assert basic_body_cells[0].text == 'a'
+        assert basic_body_cells[1].text == 'c'
