b8d65427e9431896a113510fb7498407f7a4254e
km45 <km45@users.noreply.github.com>
2019-01-06 02:53:04 +0900

    Add form value operation to handle what operates on submit

--- src/ts/components/FormikMain.tsx
+++ src/ts/components/FormikMain.tsx
@@ -1,13 +1,21 @@
 import * as Formik from 'formik';
 import * as React from 'react';
 
+enum SubmitOperation {
+    Undefined,
+    Parse,
+    Generate,
+    Open
+}
+
 interface FormValues {
+    operation: SubmitOperation;
     url: string;
 }
 
 interface Props { }
 
-function render(_: Props & Formik.FormikProps<FormValues>): JSX.Element {
+function render(props: Props & Formik.FormikProps<FormValues>): JSX.Element {
     return (
         <Formik.Form>
             <div className='ui form'>
@@ -19,22 +27,33 @@ function render(_: Props & Formik.FormikProps<FormValues>): JSX.Element {
                 </div>
                 <button
                     className='ui primary button'
-                    type='submit'
+                    type='button'
+                    onClick={async () => {
+                        await props.setFieldValue('operation', SubmitOperation.Parse);
+                        props.submitForm();
+                    }}
                 >
                     <i className='arrow alternate circle down icon' ></i>
                     parse
                 </button>
                 <button
                     className='ui secondary button'
-                    // TODO: Handle what operates on submit
-                    type='submit'
+                    type='button'
+                    onClick={async () => {
+                        await props.setFieldValue('operation', SubmitOperation.Generate);
+                        props.submitForm();
+                    }}
                 >
                     <i className='arrow alternate circle up icon' ></i>
                     generate
                 </button>
                 <button
                     className='ui positive button'
-                    type='submit'
+                    type='button'
+                    onClick={async () => {
+                        await props.setFieldValue('operation', SubmitOperation.Open);
+                        props.submitForm();
+                    }}
                 >
                     <i className='external icon' ></i>
                     open
@@ -54,7 +73,8 @@ function render(_: Props & Formik.FormikProps<FormValues>): JSX.Element {
 function mapPropsToValues(props: Props): Props & FormValues {
     console.log(props);
     return {
-        url: ''
+        url: '',
+        operation: SubmitOperation.Undefined
     };
 }
 
