d1574aac73ca1e2ecb1d2560077e87874bbd25d6
km45 <km45@users.noreply.github.com>
2019-01-06 20:12:32 +0900

    Remove old components that uses redux

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,11 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import * as ReactRedux from 'react-redux';
 
-import store from './store';
-import Form from './containers/Form';
-import Operator from './containers/Operator';
-import Visualizer from "./containers/Visualizer";
 import FormikMain from './components/FormikMain';
 
 import '../stylesheet/index.css';
@@ -13,13 +8,6 @@ import '../stylesheet/index.css';
 ReactDOM.render(
     <div>
         <FormikMain query={window.location.search} />
-        <ReactRedux.Provider store={store}>
-            <div>
-                <Form />
-                <Operator />
-                <Visualizer />
-            </div>
-        </ReactRedux.Provider>
     </div>,
     document.getElementById("content")
 );
