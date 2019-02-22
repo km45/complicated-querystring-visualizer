8aa3084b0bf51b55f5dff651ee28c1c4079775d0
Shogo Kamei <km45@users.noreply.github.com>
2019-02-23 03:25:06 +0900

    Revert "Feature/update npm packages"

--- webpack.config.js
+++ webpack.config.js
@@ -106,6 +106,11 @@ module.exports = (env, argv) => {
             entry: 'umd/react-dom.production.min.js',
             global: 'ReactDOM',
           },
+          {
+            module: 'semantic-ui-react',
+            entry: 'dist/umd/semantic-ui-react.min.js',
+            global: 'semanticUIReact',
+          },
           // Not use externals for following libraries:
           //   - ag-grid-react
           //   - pythonic
