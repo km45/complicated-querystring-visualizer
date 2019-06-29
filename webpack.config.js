33d298e004dd9cb219620395f9176f7449cea6f2
Kamei Shogo <km45@users.noreply.github.com>
2019-06-29 19:00:10 +0900

    Fix import statement for clean-webpack-plugin to resolve build error

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
