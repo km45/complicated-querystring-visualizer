e2413da112ecf3d13266e1bcd7ddc3699bc846c2
Kamei Shogo <km45@users.noreply.github.com>
2019-10-31 02:27:06 +0900

    Use AceEditor instead of ReactAce following the latest usage

--- src/ts/components/Editor.tsx
+++ src/ts/components/Editor.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import ReactAce from 'react-ace';
+import AceEditor from 'react-ace';
 
 import 'ace-builds/src-noconflict/mode-json';
 import 'ace-builds/src-noconflict/theme-textmate';
@@ -22,7 +22,7 @@ export default class Editor extends React.Component<Props, State> {
     return (
       <div>
         <h2>{this.props.title}</h2>
-        <ReactAce
+        <AceEditor
           editorProps={{ $blockScrolling: true }}
           fontSize={16}
           maxLines={Infinity}
