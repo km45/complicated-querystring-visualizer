cf6e665d345f5ac681c93533805e4873efdac125
Kamei Shogo <km45@users.noreply.github.com>
2020-01-12 00:38:56 +0900

    Add eslint-plugin-eslint-comments
    https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/README.md#plugins
    
    > ESLint comment restrictions: eslint-plugin-eslint-comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     'eslint:recommended',
     "plugin:react/recommended",
     "plugin:node/recommended",
+    "plugin:eslint-comments/recommended",
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
   ],
