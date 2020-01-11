9260c03b6a806489d652f3818d7133ba4c3885cb
Kamei Shogo <km45@users.noreply.github.com>
2020-01-12 03:53:14 +0900

    Uncomment indent 4 spaces and add note

--- .eslintrc_ts.js
+++ .eslintrc_ts.js
@@ -24,8 +24,11 @@ module.exports = {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
 
-    // // indent of 4 spaces
-    // "indent": "off",  // must disable the base rule as it can report incorrect errors
-    // "@typescript-eslint/indent": ["error", 4]
+    // indent of 4 spaces
+    //
+    // NOTE: This causes slow linting.
+    //       https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/FAQ.md#the-indent--typescript-eslintindent-rules
+    "indent": "off",  // must disable the base rule as it can report incorrect errors
+    "@typescript-eslint/indent": ["error", 4]
   }
 };
